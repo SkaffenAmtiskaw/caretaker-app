67146f5f8aaeebd14f90f44ff37d95f6558ceae7
Sarah Adams <sarah.elizabeth.adams@proton.me>
2026-06-20 14:38:47 -0500

    biome - single quotes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
-import "@mantine/core/styles.css";
+import '@mantine/core/styles.css';
 
 import {
   ColorSchemeScript,
   MantineProvider,
   mantineHtmlProps,
-} from "@mantine/core";
-import type { Metadata } from "next";
+} from '@mantine/core';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Care Circles",
-  description: "Caretaker app",
+  title: 'Care Circles',
+  description: 'Caretaker app',
 };
 
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => (
-  <html lang="en" {...mantineHtmlProps}>
+  <html lang='en' {...mantineHtmlProps}>
     <head>
       <ColorSchemeScript />
     </head>
